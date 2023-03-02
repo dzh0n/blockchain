c5146e3d67dd3de72b0487f16d291a9c87ed917c
Evgeniy Plotnikov <plotnikov.evgeniy@gmail.com>
2023-03-02 15:43:42 +0500

    Взносы для проектов и компаний

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,11 @@
 $(document).ready(function () {
+
+    $('.main-menu a').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top - 100
+        }, 2000);
+    });
     const documentsSlider = new Swiper('.documents-slider .swiper-container', {
         loop: true,
         slidesPerView: 1,
@@ -16,4 +23,18 @@ $(document).ready(function () {
             prevEl: '.history-slider .documents-button-prev',
         },
     });
+
+    $('.tariffs-col_links a').on('click', function() {
+        $(this).addClass('active').siblings().removeClass('active');
+        console.log($(this).index());
+        $(this).parents('.tariffs-col').find('.tariffs-col_tab').eq($(this).index()).addClass('active').siblings().removeClass('active');
+    });
+
+    $('.tariffs-col_btn').on('click', function () {
+        $.fancybox.open({
+            src: '#order',
+            type: 'inline'
+        });
+    });
+
 })
